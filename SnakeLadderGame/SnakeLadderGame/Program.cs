ced60e0c16c67a76f827a7130c7bcdbed3da076e
Ravi Ranjan <92181941+ravi04ez@users.noreply.github.com>
2022-06-09 23:36:32 +0530

    Add Player Reaching Winning Position

--- SnakeLadderGame/SnakeLadderGame/Program.cs
+++ SnakeLadderGame/SnakeLadderGame/Program.cs
@@ -7,9 +7,12 @@ namespace SnakeLadderGame
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake Ladder Game");
+
+            // Create object of class SnakeLadder..
             SnakeLadder ObjGame= new SnakeLadder();
+
+            // Calling method Start Game..
             ObjGame.StartGame();
-            ObjGame.DiceRoll();
             Console.ReadKey();
         }
     }
