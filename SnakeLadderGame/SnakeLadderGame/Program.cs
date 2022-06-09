dd2b3f3391a0e1fad401d89378c927c3435e7856
Ravi Ranjan <92181941+ravi04ez@users.noreply.github.com>
2022-06-10 01:12:28 +0530

    Playing Game Two Player

--- SnakeLadderGame/SnakeLadderGame/Program.cs
+++ SnakeLadderGame/SnakeLadderGame/Program.cs
@@ -9,10 +9,17 @@ namespace SnakeLadderGame
             Console.WriteLine("Welcome to Snake Ladder Game");
 
             // Create object of class SnakeLadder..
-            SnakeLadder ObjGame= new SnakeLadder();
+           // SnakeLadder ObjGame= new SnakeLadder();
 
             // Calling method Start Game..
-            ObjGame.StartGame();
+           // ObjGame.StartGame();
+
+            // Create object of class SnakeLadderGames
+            SnakeLadderGames objPlayGames =new SnakeLadderGames();
+
+            // calling the method play Games
+            objPlayGames.PlayGames();
+
             Console.ReadKey();
         }
     }
